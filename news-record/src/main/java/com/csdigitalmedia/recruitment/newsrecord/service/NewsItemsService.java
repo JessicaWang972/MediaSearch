9b886fec923ce900a7f80c09a8002736b092fe99
JessicaWang972 <104641042+JessicaWang972@users.noreply.github.com>
2022-06-15 17:21:29 +0200

    Delete news-record directory

--- news-record/src/main/java/com/csdigitalmedia/recruitment/newsrecord/service/NewsItemsService.java
+++
@@ -1,9 +0,0 @@
-package com.csdigitalmedia.recruitment.newsrecord.service;
-
-import com.csdigitalmedia.recruitment.newsrecord.entity.NewsItemsEntity;
-
-import java.util.List;
-
-public interface NewsItemsService {
-    List<NewsItemsEntity> getNewsItems();
-}
